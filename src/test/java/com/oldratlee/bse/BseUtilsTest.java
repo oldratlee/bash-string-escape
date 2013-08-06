983a3249d8ea11c0ab5ef8f5afe9f5481cab2d6d
oldratlee <oldratlee@gmail.com>
2013-08-06 19:21:35 +0800

    add test case for escapePlainString

--- src/test/java/com/oldratlee/bse/BseUtilsTest.java
+++ src/test/java/com/oldratlee/bse/BseUtilsTest.java
@@ -1,17 +1,56 @@
 package com.oldratlee.bse;
 
 import org.junit.Test;
-import static com.oldratlee.bse.BseUtils.*;
-import static org.junit.Assert.*;
+
+import static com.oldratlee.bse.BseUtils.escapePlainString;
+import static com.oldratlee.bse.BseUtils.escapeVarString;
+import static org.junit.Assert.assertEquals;
+
 /**
- * 
- * @author Jerry Lee
+ * @author Jerry Lee oldratlee(at)gmail(dot)com
  */
 public class BseUtilsTest {
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
+    private static boolean isWindows() {
+        return (OS.indexOf("win") >= 0);
+    }
+
+    static String runEcho(String escapedString) throws Throwable {
+        ProcessBuilder builder = new ProcessBuilder("bash", "-c", "echo -n " + escapedString);
+
+        Process process = builder.start();
+        OutputStreamCrawler crawler = new OutputStreamCrawler(process.getInputStream());
+
+        int exitCode = process.waitFor();
+        if (0 != exitCode) {
+            throw new IllegalStateException("exit code is not 0, not normal termination: " + crawler.getResult());
+        }
+
+        return crawler.getResult();
+    }
+
     @Test
-    public void test_escapeSimpleString() throws Exception {
-        String out = escapePlainString("foo'bar\"123");
-        assertEquals("'foo'\\''bar\"123'", out);
+    public void test_escapeSimpleString() throws Throwable {
+        String input = "abc";
+        assertEquals("'abc'", escapePlainString(input));
+
+        input = "foo'bar\"123";
+        assertEquals("'foo'\\''bar\"123'", escapePlainString(input));
+
+        if (isWindows()) return;
+
+        assertEquals(input, runEcho(escapePlainString(input)));
+        
+        String s = "Follow e space . Follow a double quotation marks \". Follow a single quotation marks '. End!";
+        assertEquals(s, runEcho(escapePlainString(s)));
+        s = "  ! $USER ${HOME} ${USER:3} \"Hello\" [    ] \t {\"k1\":\"v1\", 36, [1, \"string\"]} ";
+        assertEquals(s, runEcho(escapePlainString(s)));
+        s = "abc\n123";
+        assertEquals(s, runEcho(escapePlainString(s)));
+
+        s = "$ \r \n \t abc Java™   ";
+        assertEquals(s, runEcho(escapePlainString(s)));
     }
 
     @Test
---
+++ src/test/java/com/oldratlee/bse/OutputStreamCrawler.java
@@ -0,0 +1,40 @@
+package com.oldratlee.bse;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.InputStream;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author Jerry Lee oldratlee(at)gmail(dot)com
+ */
+class OutputStreamCrawler {
+    String result = null;
+    Throwable throwable = null;
+    CountDownLatch latch = new CountDownLatch(1);
+
+    public OutputStreamCrawler(final InputStream os) {
+        Thread crawler = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    result = IOUtils.toString(os);
+                } catch (Throwable t) {
+                    throwable = t;
+                } finally {
+                    latch.countDown();
+                }
+            }
+        });
+        crawler.setDaemon(true);
+        crawler.start();
+    }
+
+    public String getResult() throws Throwable {
+        latch.await();
+        if (throwable != null) {
+            throw throwable;
+        }
+        return result;
+    }
+}
